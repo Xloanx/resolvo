3b5d7457bbc7acbbcbcd6147edf0f1a6a0f3b0fc
Abiodun Muh'd-Ahmad Odukaye <abiodun.odukaye@mykinetic.app>
2025-01-26 16:28:48 +0100

    created mock organization api

---
+++ app/api/v1/orgs/orgAPI.js
@@ -0,0 +1,100 @@
+export const organizations = [
+    {
+        id: 1,
+        name: "Industrial Training Fund",
+        status: "active",
+        contact: "admin@itf.gov.ng"
+    },
+
+    {
+        id: 2,
+        name: "Bank of Industry",
+        status: "inactive",
+        contact: "admin@bi.gov.ng"
+    },
+
+    {
+        id: 3,
+        name: "Federal Inland Revenue Service",
+        status: "active",
+        contact: "admin@firs.gov.ng"
+    },
+
+    {
+        id: 4,
+        name: "The Presidency",
+        status: "active",
+        contact: "admin@presidency.gov.ng"
+    },
+
+    {
+        id: 5,
+        name: "National Directorate of Employment",
+        status: "inactive",
+        contact: "contact@nde.gov.ng"
+    },
+
+    {
+        id: 6,
+        name: "Small and Medium Enterprises Development Authority of Nigeria",
+        status: "active",
+        contact: "support@smedan.gov.ng"
+    },
+
+    {
+        id: 7,
+        name: "National Agency for the Control of Corruption",
+        status: "inactive",
+        contact: "contact@nacc.gov.ng"
+    },
+
+    {
+        id: 8,
+        name: "National Agency for Food and Drugs Control",
+        status: "active",
+        contact: "support@nafdac.gov.ng"
+    },
+
+    {
+        id: 9,
+        name: "Standard Organization of Nigeria",
+        status: "inactive",
+        contact: "admin@son.gov.ng"
+    },
+
+    {
+        id: 10,
+        name: "Nigeria Port Authority",
+        status: "inactive",
+        contact: "support@npa.gov.ng"
+    },
+
+    {
+        id: 11,
+        name: "Nigeria Customs Service",
+        status: "active",
+        contact: "support@nics.gov.ng"
+    },
+
+    {
+        id: 12,
+        name: "Joint Admission and Matriculation Board",
+        status: "active",
+        contact: "support@jamb.gov.ng"
+      },
+
+      {
+        id: 13,
+        name: "Nigeria National Petroleum Corporation",
+        status: "active",
+        contact: "support@nnpc.gov.ng"
+      },
+
+      {
+        id: 14,
+        name: "National Maritime Authority",
+        status: "active",
+        contact: "support@nma.gov.ng"
+      },
+    
+]
