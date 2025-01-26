3e4cd78aaba76e0b471f39125f7761da81807241
Abiodun Muh'd-Ahmad Odukaye <abiodun.odukaye@mykinetic.app>
2025-01-26 16:37:35 +0100

    edited tickets api

---
+++ app/api/v1/tickets/ticketsAPI.js
@@ -0,0 +1,184 @@
+export const tickets = [
+    {
+        id: "1",
+        status: "Open",
+        title: "Flickering UI",
+        description: "Lorem ipsum Flickering UI dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        createdAt: "Fri Sep 04 2023",
+        updatedAt:"",
+        assignedTo:[],
+        ticketer:"user1@example.com",
+        attachments:["image1.png", "image2.png"],
+        orgId: 7
+    },
+
+    {
+        id: "2",
+        status: "In_Progress",
+        title: "Password Reset",
+        description: "Lorem ipsum Password Reset dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        createdAt: "Mon Oct 24 2023",
+        updatedAt:"",
+        assignedTo:[],
+        ticketer:"user4@example.com",
+        attachments:[],
+        orgId:5
+    },
+
+    {
+        id: "3",
+        status: "Open",
+        title: "Broken Search",
+        description: "Lorem ipsum Broken Search dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        createdAt: "Sat Oct 26 2023",
+        updatedAt:"",
+        assignedTo:[],
+        ticketer:"user1@example.com",
+        attachments:["image5.png", "image6.png"],
+        orgId:6
+    },
+
+    {
+        id: "4",
+        status: "Resolved",
+        title: "Authorization Failure",
+        description: "Lorem ipsum Authorization Failure dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        createdAt: "Fri Jun 04 2023",
+        updatedAt:"",
+        assignedTo:[],
+        ticketer:"user1@example.com",
+        attachments:[],
+        orgId:3
+    },
+
+    {
+        id: "5",
+        status: "Open",
+        title: "Broken Links",
+        description: "Lorem ipsum Broken Links dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        createdAt: "Thur May 12 2024",
+        updatedAt:"",
+        assignedTo:[],
+        ticketer:"user2@example.com",
+        attachments:[],
+        orgId:4
+    },
+
+    {
+        id: "6",
+        status: "Open",
+        title: "Login Issue",
+        description: "Lorem ipsum Login Issue dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        createdAt: "Tue Feb 17 2024",
+        updatedAt:"",
+        assignedTo:[],
+        ticketer:"user6@example.com",
+        attachments:["image11.png", "image12.png"],
+        orgId:8
+    },
+
+    {
+        id: "7",
+        status: "Closed",
+        title: "App Crash",
+        description: "Lorem ipsum App Crash dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        createdAt: "Fri Feb 27 2024",
+        updatedAt:"",
+        assignedTo:[],
+        ticketer:"user4@example.com",
+        attachments:["image13.png", "image14.png"],
+        orgId:9,
+    },
+
+    {
+        id: "8",
+        status: "Resolved",
+        title: "Broken Checkout",
+        description: "Lorem ipsum Broken Checkout dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        createdAt: "Wed Apr 13 2024",
+        updatedAt:"",
+        assignedTo:[],
+        ticketer:"user35@example.com",
+        attachments:["image15.png", "image16.png"],
+        orgId:5,
+    },
+
+    {
+        id: "9",
+        status: "In_Progress",
+        title: "Checkout Errors",
+        description: "Lorem ipsum Checkout Errors dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        createdAt: "Mon Nov 29 2024",
+        updatedAt:"",
+        assignedTo:[],
+        ticketer:"user103@example.com",
+        attachments:["image17.png", "image18.png"],
+        orgId:2,
+    },
+
+    {
+        id: "10",
+        status: "Resolved",
+        title: "Delayed Email Notifcation",
+        description: "Lorem ipsum Delayed Email Notifcation dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        createdAt: "Tue Nov 16 2024",
+        updatedAt:"",
+        assignedTo:[],
+        ticketer:"user15@example.com",
+        attachments:[],
+        orgId:1,
+    },
+
+    {
+        id: "11",
+        status: "In_Progress",
+        title: "Unavailable Customer Support Chat",
+        description: "Lorem ipsum Unavailable Customer Support Chat dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        createdAt: "Mon Dec 19 2024",
+        updatedAt:"",
+        assignedTo:[],
+        ticketer:"user21@example.com",
+        attachments:[],
+        orgId:2,
+    },
+
+    {
+        id: 12,
+        status: "Open",
+        title: "Server Disturbing Beeping Sound",
+        description: "Lorem ipsum Server Disturbing Beeping Sound dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        ticketer: "user108@example.com",
+        createdAt: "Mon Dec 19 2024",
+        updatedAt: "",
+        assignedTo: [],
+        attachments:["image1.png", "image2.png"],
+        orgId:12,
+      },
+
+      {
+        id: 13,
+        status: "Open",
+        title: "CPU Fan stoppage",
+        description: "Lorem ipsum CPU Fan stoppage dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        createdAt: "Thur Jan 16 2025",
+        ticketer: "user108@example.com",
+        updatedAt: "",
+        assignedTo: [],
+        attachments:[],
+        orgId:12,
+      },
+
+      {
+        id: 14,
+        status: "Open",
+        title: "Alarm stoppage",
+        description: "Lorem ipsum Alarm stoppage dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        createdAt: "Fri Jan 23 2025",
+        ticketer: "user38@example.com",
+        updatedAt: "",
+        assignedTo: [],
+        attachments:[],
+        orgId:4
+      },
+    
+]
