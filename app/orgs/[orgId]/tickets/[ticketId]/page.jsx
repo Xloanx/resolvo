26ac76f5953eec6b93def96e69cec91b39b1e00d
Abiodun Muh'd-Ahmad Odukaye <abiodun.odukaye@mykinetic.app>
2025-02-03 20:17:54 +0100

    replaced client component with server component for dynamic ticket route

---
+++ app/orgs/[orgId]/tickets/[ticketId]/page.jsx
@@ -0,0 +1,26 @@
+import { getTicket } from "@/app/orgs/server-utils/db-interact";
+import TicketReviewForm from "./ticket-review-form";
+
+const agents = [
+    { value: 'Oliver Hansen', label: 'Oliver Hansen (Marketing)'},
+    { value: 'Van Henry', label: 'Van Henry (Finance)' },
+    { value: 'April Tucker', label: 'April Tucker (Sales)' },
+    { value: 'Ralph Hubbard', label: 'Ralph Hubbard (Operations)' },
+    { value: 'Omar Alexander', label: 'Omar Alexander (Human Resources)' },
+    { value: 'Carlos Abbott', label: 'Carlos Abbott (Development)' },
+    { value: 'Miriam Wagner', label: 'Miriam Wagner (Development)' },
+    { value: 'Bradley Wilkerson', label: 'Bradley Wilkerson (Sales)' },
+    { value: 'Virginia Andrews', label: 'Virginia Andrews (Development)' },
+    { value: 'Kelly Snyder', label: 'Kelly Snyder (Operations)' },
+  ];
+const TicketReviewPage = async ({params}) => {
+    const {orgId, ticketId} = await (params)
+    const response = await getTicket(orgId, ticketId);
+
+    //stringify and parse the data, forcing it into a plain object format.
+    //to be consumed by client component without returning error
+    const ticket = JSON.parse(JSON.stringify(response)); 
+    return <TicketReviewForm ticket={ticket}/>
+}
+ 
+export default TicketReviewPage;
