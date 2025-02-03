ee81b098e8f949fc98cf8ff8911a88378431a257
Abiodun Muh'd-Ahmad Odukaye <abiodun.odukaye@mykinetic.app>
2025-02-03 20:19:04 +0100

    replaced CC with SC for ticket creation

--- app/orgs/[orgId]/tickets/new/page.jsx
+++ app/orgs/[orgId]/tickets/new/page.jsx
@@ -1,138 +1,35 @@
-"use client"
-import { useState, use } from "react"
-import { useRouter } from "next/navigation"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+'use client'
+import { redirect } from "next/navigation"
+import SubmitButton from "../components/submit-button";
+import { addTicket } from '@/actions/tickets';
+import { use, useEffect, useActionState } from "react";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea"
 import { MdDelete, MdAutoDelete } from "react-icons/md";
+import { ToastContainer, toast, Bounce } from 'react-toastify';
 import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-  } from "@/components/ui/form"
-  import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
+Card,
+CardContent,
+CardDescription,
+CardFooter,
+CardHeader,
+CardTitle,
+} from "@/components/ui/card"
 
 
-
-const BASE_URL = "http://localhost:3000"
-
-
-const formSchema = z.object({
-    email: z.optional(z.string().email()),
-    title: z.string().min(2, {
-      message: "Title must be at least 2 characters.",
-    }),
-    description: z.string().min(10, {
-        message: "Description must be at least 10 characters.",
-      }),
-
-  })
 
 
 const NewTicketUI = ({params}) => {
-    const {orgId} = use(params)
-    const [loading, setLoading] = useState(false);
+    const {orgId} = use (params)
 
-    const router = useRouter()
-
-    // Define your form.
-    const form = useForm({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-        email:"",
-        title: "",
-        description:"",
-        uploads:[],
-        },
-    })
-
-    // Define a submit handler.
-    async function onSubmit(values) {
-        // Do something with the form values.
-        setLoading(true)
-        const {email:ticketer, title, description, uploads:attachments} = values
-        try {
-            const response = await fetch("http://localhost:3000/api/v1/tickets", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(title, description, ticketer, attachments, orgId),
-            });
-
-            if (response.ok) {
-                router.push("/org/[orgId]/tickets")
-            }
-        } catch (error) {
-            console.error("Error:", error);
-        } finally {
-            setLoading(false)
-        }
-    }
-
-    //file upload handler
-    const handleFileChange = (event) => {
-        const files = Array.from(event.target.files);
-        setUploadedFiles(files);
-        // setValue("files", files); // Update form value
+    const initialState = {
+        success: null, 
+        errors: {},
       };
+    
+      //make orgId available for addTicket server action
+    const addTicketWithId = addTicket.bind(null, orgId); 
 
-    const handleRemoveFile = () =>{
-        console.log('deleted')
-    }
- 
-    //   const updateFiles = (incomingFiles) => {
-    //     console.log("incoming files", incomingFiles);
-    //     setExtFiles(incomingFiles);
-    //   };
-    //   const onDelete = (id) => {
-    //     setExtFiles(extFiles.filter((x) => x.id !== id));
-    //   };
-    //   const handleSee = (imageSource) => {
-    //     setImageSrc(imageSource);
-    //   };
-    //   const handleWatch = (videoSource) => {
-    //     setVideoSrc(videoSource);
-    //   };
-    //   const handleStart = (filesToUpload) => {
-    //     console.log("advanced demo start upload", filesToUpload);
-    //   };
-    //   const handleFinish = (uploadedFiles) => {
-    //     console.log("advanced demo finish upload", uploadedFiles);
-    //   };
-    //   const handleAbort = (id) => {
-    //     setExtFiles(
-    //       extFiles.map((ef) => {
-    //         if (ef.id === id) {
-    //           return { ...ef, uploadStatus: "aborted" };
-    //          } else return { ...ef };
-    //        })
-    //      );
-    //   };
-    //   const handleCancel = (id) => {
-    //     setExtFiles(
-    //       extFiles.map((ef) => {
-    //         if (ef.id === id) {
-    //           return { ...ef, uploadStatus: undefined };
-    //          } else return { ...ef };
-    //        })
-    //      );
-    //   };
-
+    const [state, formAction] = useActionState(addTicketWithId, initialState);
     function FileIcon(props) {
         return (
           <svg
@@ -153,28 +50,57 @@ const NewTicketUI = ({params}) => {
         )
       }
       
-      
-      function XIcon(props) {
-        return (
-          <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M18 6 6 18" />
-            <path d="m6 6 12 12" />
-          </svg>
-        )
-      }
+    function XIcon(props) {
+    return (
+        <svg
+        {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        >
+        <path d="M18 6 6 18" />
+        <path d="m6 6 12 12" />
+        </svg>
+    )
+    }
 
 
+    // Trigger toast notification when state changes
+    useEffect(() => {
+        if (state?.success) {
+                    toast.success('Ticket submitted Successfully!', {
+                        position: "top-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: false,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light",
+                        transition: Bounce,
+                        });
+        } else if (state?.success === false) {
+                    toast.error('Ticket submission failed!', {
+                        position: "top-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: false,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light",
+                        transition: Bounce,
+                        });
+        }
+    }, [state]);
+
+    
     return ( 
         <>
             <div className="grid grid-rows-[20px_1fr_20px] items-center 
@@ -187,125 +113,106 @@ const NewTicketUI = ({params}) => {
                     <CardDescription>Create your Ticket here. Click submit when you're done.</CardDescription>
                 </CardHeader>
                 <CardContent>
-                <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-
-                    <FormField
-                        control={form.control}
-                        name="email"
-                        render={({ field }) => (
-                            <FormItem>
-                                <div className="grid grid-cols-4 items-center gap-4">
-                                    <FormLabel>Email</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="Let's reach back to you..." {...field} 
-                                        className="col-span-3" 
-                                        />
-                                    </FormControl>
-                                </div>
-                            <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-
-
-                    <FormField
-                        control={form.control}
-                        name="title"
-                        render={({ field }) => (
-                            <FormItem>
-                                <div className="grid grid-cols-4 items-center gap-4">
-                            <FormLabel>Title</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Ticket title here..." {...field} 
-                                className="col-span-3"
-                                />
-                            </FormControl>
-                                </div>
-                            <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-
-                    <FormField
-                        control={form.control}
-                        name="description"
-                        render={({ field }) => (
-                            <FormItem>
-                                <div className="grid grid-cols-4 items-center gap-4">
-                                    <FormLabel>Description</FormLabel>
-                                    <FormControl>
-                                        <Textarea
-                                        placeholder="Provide more details about this ticket"
-                                        className="resize-none col-span-3"
-                                        rows={5}
-                                        {...field}
-                                        />
-                                    </FormControl>
-                                </div>
-                            <FormMessage />
-                            </FormItem>
-                        )}
-                        />
-
-                        <FormField
-                            control={form.control}
-                            name="upload"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Supporting File(s)</FormLabel>
-                                    <FormControl>
-                                        <Input id="upload" 
-                                        name="upload"
-                                        type="file" 
-                                        multiple 
-                                        onChange={handleFileChange}
-                                        accept="image/*,application/pdf"/>
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                            />
-
-                            <div className="border-dashed border rounded-lg p-4 grid grid-cols-4 items-center gap-4">
-                            <div className="col-span-2">
-                                <div className="grid gap-1.5 text-sm">
-                                    <FileIcon className="h-6 w-6" />
-                                    <div>example-document.pdf <span>(5.2MB)</span></div>
-                                </div>
-                            </div>
-                            <div className="col-span-2 text-right">
-                                <Button variant="ghost" size="sm" onClick={handleRemoveFile}>
-                                    <MdDelete className="h-4 w-4" />
-                                    <span className="sr-only">Remove</span>
-                                </Button>
-                            </div>
-                            </div>
-
-                    <Button type="submit"  className ="w-full" disabled={loading}>
-                        
-                        {loading? "Submitting..." : "Submit Ticket"}
-                        
-                    </Button>
+                <form action={formAction} className="space-y-8">
 
                     <div className="grid grid-cols-4 items-center gap-4">
-                        <div className="col-span-2" onClick={() => router.back()}>
+                        <label className=" text-right">
+                            Email
+                        </label>
+                        <input
+                            type="text"
+                            className="block w-full p-2 text-black border rounded col-span-3"
+                            name="ticketer"
+                            placeholder="Let's reach back to you..."
+                        />
+                        
+                    </div>
+                    
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <label className=" text-right">
+                            Title
+                        </label>
+                        <input
+                            type="text"
+                            className="block w-full p-2 text-black border rounded col-span-3"
+                            name="title"
+                            placeholder="Ticket title here..."
+                        />
+                        <div></div>
+                        {state?.title && (
+                            <p className="text-red-500 col-span-3 text-left">{state.title}</p>
+                        )}
+                    </div>
+
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <label className="text-right">
+                            Description
+                        </label>
+                        <textarea
+                            className="block w-full p-2 text-black border 
+                                        rounded col-span-3 "
+                            name="description"
+                            placeholder="Provide more details about this ticket"
+                            row="5"
+                        />
+                        <div></div>
+                        {state?.description && (
+                        <p className="text-red-500 col-span-3 text-left">{state.description}</p>
+                        )}
+                    </div>
+
+                    <input type="hidden" name="orgId" value={orgId} />
+
+                    <div className="grid grid-cols-4 items-center gap-4">
+                    <label className="col-span-4">
+                            Support Documents
+                        </label>
+                        <input
+                            type="file"
+                            name="docs"
+                            multiple 
+                            accept="image/*,application/pdf"
+                            className="block w-full p-2 text-black border rounded col-span-4"
+                            
+                        />
+                        
+                    </div>
+
+                    <div className="col-span-4"> Added Documents</div>
+                    {/* <div className="border-dashed border rounded-lg p-4 grid grid-cols-4 items-center gap-4">
+                        
+                        <div className="col-span-2">
+                            <div className="flex gap-1.5 text-sm">
+                                <FileIcon className="h-6 w-6" />
+                                <div>example-document.pdf <span>(5.2MB)</span></div>
+                            </div>
+                        </div>
+                        <div className="col-span-2 text-right">
+                            <Button variant="ghost" size="sm" >
+                                <MdDelete className="h-4 w-4" />
+                                <span className="sr-only">Remove</span>
+                            </Button>
+                        </div>
+                    </div> */}
+
+                    <SubmitButton />
+
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <div className="col-span-2" >
                             <Button variant="outline">Back</Button>
                         </div>
                         <div className="col-span-2 text-right">
-                            <Button variant="outline" onClick={() => form.reset()} >Reset</Button>
+                            <Button variant="outline"  >Reset</Button>
                         </div>
                     </div>
                 </form>
-            </Form>
+
+                
                 </CardContent>
-                {/* <CardFooter className="flex justify-between">
-                    <Button variant="outline">Cancel</Button>
-                    <Button>Deploy</Button>
-                </CardFooter> */}
                     </Card>
                 </main>   
             </div>
+            <ToastContainer />
         </>
     );
 }
