86d1e576eac4b05f3c0847928be704903690eaaf
Abiodun Muh'd-Ahmad Odukaye <abiodun.odukaye@mykinetic.app>
2025-02-03 20:34:11 +0100

    Implemented table actions for organizations' table on resolvo superuser dashboard

---
+++ app/orgs/[orgId]/tickets/components/table-actions.jsx
@@ -0,0 +1,59 @@
+import { Button } from "@/components/ui/button"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import Link from "next/link";
+
+const TableActions = ({organization}) => {
+    return ( 
+<DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost">...</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+          <Link href={`/orgs/${organization._id}`} target="_blank" rel="noopener noreferrer">
+            View Profile
+            </Link>
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Billing
+            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+          </DropdownMenuItem>
+
+          <DropdownMenuItem>
+            Settings
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href={`/orgs/${organization._id}/tickets/`} target="_blank" rel="noopener noreferrer">
+              View Tickets' Page
+            </Link>
+            <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+     );
+}
+ 
+export default TableActions;
+
+
