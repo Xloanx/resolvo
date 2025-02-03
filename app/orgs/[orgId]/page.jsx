7f9f1379a76b2868858226f2f2ed13d57b269398
Abiodun Muh'd-Ahmad Odukaye <abiodun.odukaye@mykinetic.app>
2025-02-03 20:25:41 +0100

    edited styling for dynamic organization dashboard

--- app/orgs/[orgId]/page.jsx
+++ app/orgs/[orgId]/page.jsx
@@ -4,25 +4,23 @@ import { buttonVariants } from "@/components/ui/button"
 import Link from "next/link";
 
 const Dashboard = async ({params}) => {
-    const {orgId} = params;
+    const {orgId} = await params;
     const res = await fetch(`http://localhost:3000/api/v1/orgs/${orgId}`)
     const data = await res.json()
-    console.log(data.organization);
+    console.log(data);
 
 
     return ( 
     <>
     <div> {data.organization.name} Dashboard</div>
     <div className="grid items-center justify-center gap-4">
-        <div className="items-center justify-center">
-    <p>Report a flawed service delivery @ {data.organization.name}.</p>
-    </div>
-    <div className="items-center justify-center">
-    {/* <DialogUI /> */}
-    <Link className={buttonVariants({ variant: "outline" })} href={`/orgs/${orgId}/tickets/new`}>Click here</Link>
-
-    </div>
-    
+        <div >
+            <p>Report a flawed service delivery @ {data.organization.name}.</p>
+        </div>
+        <div >
+            {/* <DialogUI /> */}
+            <Link className={buttonVariants({ variant: "outline" })} href={`/orgs/${orgId}/tickets/new`}>Click here</Link>
+        </div>
     </div>
     </>
         
