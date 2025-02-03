559337ae8b84369843c5af4d2a5a5a136c41a02b
Abiodun Muh'd-Ahmad Odukaye <abiodun.odukaye@mykinetic.app>
2025-02-03 20:10:39 +0100

    edited error component for tickets feed

--- app/orgs/[orgId]/tickets/error.jsx
+++ app/orgs/[orgId]/tickets/error.jsx
@@ -1,14 +1,18 @@
 "use client";
 import { useEffect } from "react";
+import { Button } from "@/components/ui/button"
 
 export default function ErrorPage({ error, reset }) {
   useEffect(() => {
     console.error(`${error}`);
   }, [error]);
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="grid items-center justify-center gap-4">
       <div className="text-2xl text-red-500">Error fetching tickets</div>
-      <button onClick={() => reset()}>Try again</button>
+      <div>
+      <Button variant="outline" onClick={() => reset()}>Try Again</Button>
+
+      </div>
     </div>
   );
 }
